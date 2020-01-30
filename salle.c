4565f8655f9c6dc6cc234bb11a6e02cb3217fe18
Axel <axelj@live.fr>
2020-01-30 14:21:35 +0100

    Génération d'une salle avec portes aléatoires

--- salle.c
+++ salle.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define TAILLE_LABY 23
 
@@ -45,31 +46,77 @@ void init_salle(int salle[TAILLE_LABY][TAILLE_LABY]){
   }
 }
 
-void ajout_porte_salle(int salle[TAILLE_LABY][TAILLE_LABY]){
+/**
+ * \fn init_salle(int salle[TAILLE_LABY][TAILLE_LABY])
+ * \brief fonction qui ajoute une porte à un côté du carré
+ * \param salle[TAILLE_LABY][TAILLE_LABY] est le tableau de TAILLE_LABY qui comprend la salle
+ * \param direction correspond à un nombre entre 0 et 3 indiquant quel mur est seléctionné.
+ */
+void ajout_porte_salle(int salle[TAILLE_LABY][TAILLE_LABY], int direction){
   int millieu = TAILLE_LABY / 2;
   for(int i = 0; i<TAILLE_LABY; i++){
     for(int j=0; j<TAILLE_LABY; j++){
-      if(i == 0){
-        if(j == millieu-1 || j == millieu || j== millieu+1){
-          salle[i][j]=2;
+
+      //Nord
+      if(direction == 0){
+        if(i == 0){
+          if(j == millieu-1 || j == millieu || j== millieu+1){
+            salle[i][j]=2;
+          }
         }
       }
-      else if(j == 0){
-        if(i == millieu-1 || i == millieu || i== millieu+1){
-          salle[i][j]=2;
+      if(direction == 1){
+        if(j == 0){
+          if(i == millieu-1 || i == millieu || i== millieu+1){
+            salle[i][j]=2;
+          }
         }
       }
-      else if(j == TAILLE_LABY-1){
-        if(i == millieu-1 || i == millieu || i== millieu+1){
-          salle[i][j]=2;
+
+      //Sud
+      if(direction == 2){
+        if(j == TAILLE_LABY-1){
+          if(i == millieu-1 || i == millieu || i== millieu+1){
+            salle[i][j]=2;
+          }
         }
       }
-      if(i == TAILLE_LABY-1){
-        if(j == millieu-1 || j == millieu || j== millieu+1){
-          salle[i][j]=2;
+
+      //Est
+      if(direction == 3){
+        if(i == TAILLE_LABY-1){
+          if(j == millieu-1 || j == millieu || j== millieu+1){
+            salle[i][j]=2;
+          }
         }
       }
     }
+  }
+}
+
+void aleatoire_porte(int salle[TAILLE_LABY][TAILLE_LABY]){
+  srand(time(NULL));
+  int alea;
+
+  //Nord
+  alea = rand()%9;
+  if(alea > 5){
+    ajout_porte_salle(salle, 0);
+  }
+  //Ouest
+  alea = rand()%9;
+  if(alea > 5){
+    ajout_porte_salle(salle, 1);
+  }
+  //Sud
+  alea = rand()%9;
+  if(alea > 5){
+    ajout_porte_salle(salle, 2);
+  }
+  //Est
+  alea = rand()%9;
+  if(alea > 5){
+    ajout_porte_salle(salle, 3);
   }
 }
 
@@ -81,7 +128,7 @@ void ajout_porte_salle(int salle[TAILLE_LABY][TAILLE_LABY]){
 void afficher_salle(int salle[TAILLE_LABY][TAILLE_LABY]){
   for(int i = 0; i<TAILLE_LABY; i++){
     for(int j=0; j<TAILLE_LABY; j++){
-      if(salle[i][j]==0){
+      if(salle[i][j]==0 || salle[i][j]==2){
         printf("  ");
       }
       else{
@@ -90,13 +137,4 @@ void afficher_salle(int salle[TAILLE_LABY][TAILLE_LABY]){
     }
     printf("\n");
   }
-}
-
-int main(){
-  int salle[TAILLE_LABY][TAILLE_LABY];
-
-init_salle(salle);
-ajout_porte_salle(salle);
-
-afficher_salle(salle);
 }
