a815315caa8946d83f447a005c9e9ffded4325be
Nevens-fr <59256819+Nevens-fr@users.noreply.github.com>
2020-02-04 00:29:57 +0100

    Delete initialisation_sdl_fonctions.h

--- projet_Roguelike/initialisation_sdl_fonctions.h
+++
@@ -1,7 +0,0 @@
-//fonctions de la sdl pour son initialisation, fichier .h
-
-
-int initialisation_sdl(SDL_Window **window, SDL_Renderer **rendu);
-void quit_sdl(SDL_Renderer **rendu, SDL_Window **window);
-int init_or_quit_ttf(int action);
-extern void get_text_and_rect(SDL_Renderer *renderer, int x, int y, char *text,TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect);
