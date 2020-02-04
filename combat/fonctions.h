0c2f0d6f0b5c91f3b4bf70d2e880074de5bf8017
TimoxR2 <60466122+TimoxR2@users.noreply.github.com>
2020-02-04 11:47:19 +0100

    Add files via upload

--- combat/fonctions.h
+++ combat/fonctions.h
@@ -2,4 +2,18 @@ carte_t * creer_carte(char *, type_carte, int *, int);
 
 perso_t * creer_perso();
 
+ennemi_t * creer_ennemi(char *);
+
+void detruire_carte(carte_t **);
+
 void detruire_perso(perso_t **);
+
+void detruire_ennemi(ennemi_t **);
+
+int initiative (perso_t *, ennemi_t *);
+
+void tour_ennemi(perso_t *, ennemi_t *);
+
+int tour_perso(int, perso_t *, ennemi_t *, carte_t **);
+
+void combat(perso_t *, ennemi_t *, carte_t **);
