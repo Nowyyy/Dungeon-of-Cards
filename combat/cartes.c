d481b45c0ccc612894efa133f6dc2bf6c594f171
TimoxR2 <60466122+TimoxR2@users.noreply.github.com>
2020-02-04 10:10:21 +0100

    Update cartes.c

--- combat/cartes.c
+++ combat/cartes.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "structures.h"
+#include "fonctions.h"
 /*!
 * \file cartes.c
 * \brief Ce programme contient les fonctions liées aux cartes
 * \author Timothée Marin
 * \version 0.1
-* \date 30 janvier 2020
+* \date 30 janvier 2020œ
 *
 *
 *
 *
 */
-#define TAILLE 20
-#define TAILLE_DECK 5
 
 /**
 * \fn carte_t * creer_carte(char * nom, type_carte type, int * cible, int valeur)
@@ -25,47 +25,24 @@
 * \return i+2
 */
 
-typedef enum {ATTAQUE = -1, DEFENSE = 1} type_carte ;
-
-typedef struct carte_s {
-  char * nom ;
-  type_carte type;
-  int * cible;
-  int valeur;
-} carte_t ;
-
-carte_t * creer_carte(char * nom, type_carte type, int * cible, int valeur){
-  carte_t * carte = NULL ;
-  static unsigned long int cpt = 0 ;
-
-  carte = malloc(sizeof(carte_t));
-
-  carte->nom = malloc(sizeof(char)*TAILLE);
-  strcpy(carte->nom , nom);
-  carte->valeur = valeur;
-  carte->cible = cible;
-  carte->type = type;
-
-  return(carte);
-}
-
-
 int main(){
-  int pv_joueur = 20;
-  int pv_ennemi = 20;
+  int pv = 20;
+  perso_t * perso = creer_perso();
   carte_t * deck[TAILLE_DECK];
-  deck[0] = creer_carte("soin", DEFENSE, &pv_joueur, 10);
-  deck[1] = creer_carte("épée", ATTAQUE, &pv_ennemi, 10);
-  deck[2] = creer_carte("épée", ATTAQUE, &pv_ennemi, 10);
-  deck[3] = creer_carte("épée", ATTAQUE, &pv_ennemi, 10);
-  deck[4] = creer_carte("boule de feu", ATTAQUE, &pv_ennemi, 20);
+  deck[0] = creer_carte("soin", DEFENSE, &(perso->pv), 10);
+  deck[1] = creer_carte("épée", ATTAQUE, &pv, 10);
+  deck[2] = creer_carte("épée", ATTAQUE, &pv, 10);
+  deck[3] = creer_carte("épée", ATTAQUE, &pv, 10);
+  deck[4] = creer_carte("boule de feu", ATTAQUE, &pv, 20);
   int i;
   for(i=0;i<5;i++){
     *(deck[i]->cible) += deck[i]->type * deck[i]->valeur;
     printf("Tour %d : \n", i+1);
     printf("Carte utilisée : %s\n", deck[i]->nom);
     printf("%d\n", deck[i]->type * deck[i]->valeur);
-    printf("pv joueur : %d\n", pv_joueur);
-    printf("pv ennemi : %d\n\n", pv_ennemi);
+    printf("pv joueur : %d\n", perso->pv);
+    printf("pv ennemi : %d\n\n", pv);
   }
+  detruire_perso(&perso);
 }
+
