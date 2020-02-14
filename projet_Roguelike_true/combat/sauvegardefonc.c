95374f2b315ca26ecad692bbc6e9fbeeafe4a228
Malabry Thomas <60466108+Thomas-Malabry@users.noreply.github.com>
2020-02-14 11:34:22 +0100

    Update sauvegardefonc.c

--- projet_Roguelike_true/combat/sauvegardefonc.c
+++ projet_Roguelike_true/combat/sauvegardefonc.c
@@ -15,15 +15,56 @@
 *\brief Fonction qui permet de sauvegarde toutes les données d'une carte
 *\param deck[] Permet de sauvegarder les cartes d'un deck
 */
-void savecarte (carte_t* deck[]){
+void savecarte (carte_t* carte2){
   FILE *fichier=NULL;
   fichier=fopen("sauvegarde.txt","w");
+  en_tete();
+  if (fichier != NULL)
+   {
+      while(!hors_liste()){
+        valeur_elt(carte2);
+        fprintf(fichier,"%d %d %d %s\n",carte2->type,carte2->valeur,carte2->consommable,carte2->nom);
+        sui
+   }
+   else
+   {
+       // On affiche un message d'erreur si on veut
+       printf("Impossible d'ouvrir le fichier sauvegarde.txt");
+   }
+   fclose(fichier);
+/**
+*\fn void readcarte(carte_t* deck2[])
+*\brief Fonction qui permet de lire une carte
+*\param deck2[] Permet de lire les cartes d'un deck
+*/
+void readcarte(carte_t* deck2[]){
+  FILE *fichier=NULL;
+  fichier=fopen("sauvegarde.txt","r");
   int i;
   if (fichier != NULL)
    {
        for(i=0;i<5;i++){
-         fprintf(fichier,"%d %d %s\n",deck[i]->valeur,deck[i]->type,deck[i]->nom);
+         fscanf(fichier,"%d %d %[^\n]",&deck2[i]->valeur ,&deck2[i]->type,deck2[i]->nom);
        }
+   }
+   else
+   {
+       // On affiche un message d'erreur si on veut
+       printf("Impossible d'ouvrir le fichier sauvegarde.txt");
+   }
+   fclose(fichier);
+}
+/**
+*\fn void saveperso (perso_t *perso)
+*\brief Fonction qui permet de sauvegarder les données du personnage
+*\param perso Pour savoir les données du perso à sauvegarder
+*/
+void saveperso (perso_t *perso){
+  FILE *fichier=NULL;
+  fichier=fopen("perso.txt","w");
+  if (fichier != NULL)
+   {
+       fprintf(fichier,"%d %d %d %d %d %d\n",perso->pv,perso->vitesse,perso->attaque,perso->defense,perso->x,perso->y);
    }
    else
    {
@@ -33,6 +74,35 @@ void savecarte (carte_t* deck[]){
    fclose(fichier);
 
 }
+/**
+*\fn void readperso(perso_t*perso)
+*\brief Fonction qui permet de lire les données du personnage
+*\param perso Pour savoir les données du perso à sauvegarder
+*/
+
+void readperso(perso_t*perso){
+  FILE *fichier=NULL;
+  fichier=fopen("perso1.txt","r");
+  if (fichier != NULL)
+   {
+       fscanf(fichier,"%d %d %d %d %d %d\n",&perso->pv,&perso->vitesse,&perso->attaque,&perso->defense,&perso->x,&perso->y);
+   }
+   else
+   {
+       // On affiche un message d'erreur si on veut
+       printf("Impossible d'ouvrir le fichier sauvegarde.txt");
+   }
+   fclose(fichier);
+}vant();
+      }
+
+   }
+   else
+   {
+       // On affiche un message d'erreur si on veut
+       printf("Impossible d'ouvrir le fichier sauvegarde.txt");
+   }
+   fclose(fichier);
 /**
 *\fn void readcarte(carte_t* deck2[])
 *\brief Fonction qui permet de lire une carte
