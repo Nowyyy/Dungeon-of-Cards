8f1fda46b3af8f3e7416f0c444f79765bd553361
Malabry <thomas.malabry72@gmail.com>
2020-03-15 22:33:51 +0100

    Rectangle s'affiche mais ne se déplace pas

--- projet_Roguelike_true/Src/combat_tour_par_tour.c
+++ projet_Roguelike_true/Src/combat_tour_par_tour.c
@@ -15,102 +15,6 @@
 #include "../include/initialisation_sdl_fonctions.h"
 #include "../include/fonctions.h"
 #include "../include/clavier.h"
-/**
-*\fn void affichage_combat_personnage(SDL_Renderer *rendu,perso_t *pers, ennemi_t * ennemi)
-
-*\param *ennemi, la structure contenant tous les ennemis
-*\param pers, la structure contenant le personnage
-*\param *rendu, le renderer sur lequel on dessine
-
-*\brief Permet d'afficher toutes la partie combat
-
-*/
-void affichage_combat_personnage(SDL_Renderer *rendu,perso_t *pers, ennemi_t * ennemi,SDL_Texture *defausse_texture ,SDL_Texture *fuir_texture,SDL_Rect defausse_rect ,SDL_Rect fuir_rect){
-  //écran noir puis nettoie l'écran
-  SDL_SetRenderDrawColor(rendu,0,0,0,255);
-  SDL_RenderClear(rendu);
-  image_t images[NB_TEXTURES];
-  /*Mise en place des fonds*/
-  charge_image(FOND_COMBAT_PATH,&images[fond2], rendu);
-  images[fond2].rectangle.x= -50;
-  images[fond2].rectangle.y= -415;
-  images[fond2].rectangle.w *= 2;
-  images[fond2].rectangle.h *= 2;
-  SDL_RenderCopy(rendu, images[fond2].img, NULL, &images[fond2].rectangle);
-
-  charge_image(COMBAT_PATH,&images[fond], rendu);
-  images[fond].rectangle.x=0;
-  images[fond].rectangle.y= 450;
-  images[fond].rectangle.w *= 4;
-  SDL_RenderCopy(rendu, images[fond].img, NULL, &images[fond].rectangle);
-
-  /*Mise en place des cartes */
-  charge_image(CARTE1_PATH,&images[carte1], rendu);
-  images[carte1].rectangle.x=50;
-  images[carte1].rectangle.y= 450;
-  images[carte1].rectangle.w /=8;
-  images[carte1].rectangle.h /=8;
-  SDL_RenderCopy(rendu, images[carte1].img, NULL, &images[carte1].rectangle);
-
-  charge_image(CARTE2_PATH,&images[carte2], rendu);
-  images[carte2].rectangle.x=250;
-  images[carte2].rectangle.y= 450;
-  images[carte2].rectangle.w /=2;
-  images[carte2].rectangle.h /=2;
-  SDL_RenderCopy(rendu, images[carte2].img, NULL, &images[carte2].rectangle);
-
-  charge_image(CARTE3_PATH,&images[carte3], rendu);
-  images[carte3].rectangle.x=450;
-  images[carte3].rectangle.y= 450;
-  images[carte3].rectangle.w /=2;
-  images[carte3].rectangle.h /=2;
-  SDL_RenderCopy(rendu, images[carte3].img, NULL, &images[carte3].rectangle);
-
-  charge_image(CARTE4_PATH,&images[carte4], rendu);
-  images[carte4].rectangle.x=650;
-  images[carte4].rectangle.y= 450;
-  images[carte4].rectangle.w /=2;
-  images[carte4].rectangle.h /=2;
-  SDL_RenderCopy(rendu, images[carte4].img, NULL, &images[carte4].rectangle);
-
-  /*Texte Défausse et fuir*/
-  SDL_RenderCopy(rendu, defausse_texture, NULL, &defausse_rect);
-  SDL_RenderCopy(rendu, fuir_texture, NULL, &fuir_rect);
-
-  /*Mise en place du personnage*/
-  int w=pers->sprites[idle_droite].rectangle.w;
-  int h=pers->sprites[idle_droite].rectangle.h;
-  int x=pers->sprites[idle_droite].rectangle.x;
-  int y=pers->sprites[idle_droite].rectangle.y;
-  pers->sprites[idle_droite].rectangle.w *=3;
-  pers->sprites[idle_droite].rectangle.h *=3;
-  pers->sprites[idle_droite].rectangle.x = 150;
-  pers->sprites[idle_droite].rectangle.y = 250;
-  pers->sprites[courant] = pers->sprites[idle_droite];
-  SDL_RenderCopy(rendu, pers->sprites[courant].img, NULL, &pers->sprites[0].rectangle);
-
-  /*Afficher l'ennemi*/
-  int xe=ennemi->sprites[0].rectangle.x;
-  int ye=ennemi->sprites[0].rectangle.y;
-  int he=ennemi->sprites[0].rectangle.h;
-  int we=ennemi->sprites[0].rectangle.w;
-  ennemi->sprites[0].rectangle.x = 750;
-  ennemi->sprites[0].rectangle.y = 240;
-  ennemi->sprites[0].rectangle.w *= 3;
-  ennemi->sprites[0].rectangle.h *= 3;
-  ennemi->sprites[0] = ennemi->sprites[0];
-  SDL_RenderCopy(rendu, ennemi->sprites[0].img, NULL, &ennemi->sprites[0].rectangle);
-
-  SDL_RenderPresent(rendu);
-  pers->sprites[idle_droite].rectangle.w = w;
-  pers->sprites[idle_droite].rectangle.h = h;
-  pers->sprites[idle_droite].rectangle.x = x+10;
-  pers->sprites[idle_droite].rectangle.y = y+10;
-  ennemi->sprites[0].rectangle.w = we;
-  ennemi->sprites[0].rectangle.h = he;
-  ennemi->sprites[0].rectangle.x = xe-50;
-  ennemi->sprites[0].rectangle.y = ye+50;
-}
 
 
 
@@ -163,12 +67,103 @@ int deplacement_rectangle_selection_combat(SDL_Rect defausse, SDL_Rect fuir, SDL
 
 
 
+/**
+*\fn void affichage_combat_personnage(SDL_Renderer *rendu,perso_t *pers, ennemi_t * ennemi)
 
+*\param *ennemi, la structure contenant tous les ennemis
+*\param pers, la structure contenant le personnage
+*\param *rendu, le renderer sur lequel on dessine
 
+*\brief Permet d'afficher toutes la partie combat
 
+*/
+void affichage_combat_personnage(SDL_Renderer *rendu,perso_t *pers, ennemi_t * ennemi,SDL_Texture *defausse_texture ,SDL_Texture *fuir_texture,SDL_Rect defausse_rect ,SDL_Rect fuir_rect,SDL_Rect *rect_sel){
+  //écran noir puis nettoie l'écran
+  SDL_SetRenderDrawColor(rendu,0,0,0,255);
+  SDL_RenderClear(rendu);
+  image_t images[NB_TEXTURES];
+  /*Mise en place des fonds*/
+  charge_image(FOND_COMBAT_PATH,&images[fond2], rendu);
+  images[fond2].rectangle.x= -50;
+  images[fond2].rectangle.y= -415;
+  images[fond2].rectangle.w *= 2;
+  images[fond2].rectangle.h *= 2;
+  SDL_RenderCopy(rendu, images[fond2].img, NULL, &images[fond2].rectangle);
 
+  charge_image(COMBAT_PATH,&images[fond], rendu);
+  images[fond].rectangle.x=0;
+  images[fond].rectangle.y= 450;
+  images[fond].rectangle.w *= 4;
+  SDL_RenderCopy(rendu, images[fond].img, NULL, &images[fond].rectangle);
 
+  /*Mise en place des cartes */
+  charge_image(CARTE1_PATH,&images[carte1], rendu);
+  images[carte1].rectangle.x=50;
+  images[carte1].rectangle.y= 450;
+  images[carte1].rectangle.w /=8;
+  images[carte1].rectangle.h /=8;
+  SDL_RenderCopy(rendu, images[carte1].img, NULL, &images[carte1].rectangle);
 
+  charge_image(CARTE2_PATH,&images[carte2], rendu);
+  images[carte2].rectangle.x=250;
+  images[carte2].rectangle.y= 450;
+  images[carte2].rectangle.w /=2;
+  images[carte2].rectangle.h /=2;
+  SDL_RenderCopy(rendu, images[carte2].img, NULL, &images[carte2].rectangle);
+
+  charge_image(CARTE3_PATH,&images[carte3], rendu);
+  images[carte3].rectangle.x=450;
+  images[carte3].rectangle.y= 450;
+  images[carte3].rectangle.w /=2;
+  images[carte3].rectangle.h /=2;
+  SDL_RenderCopy(rendu, images[carte3].img, NULL, &images[carte3].rectangle);
+
+  charge_image(CARTE4_PATH,&images[carte4], rendu);
+  images[carte4].rectangle.x=650;
+  images[carte4].rectangle.y= 450;
+  images[carte4].rectangle.w /=2;
+  images[carte4].rectangle.h /=2;
+  SDL_RenderCopy(rendu, images[carte4].img, NULL, &images[carte4].rectangle);
+  /*afficher le rectangle*/
+  SDL_RenderDrawRect(rendu, rect_sel);
+  /*Texte Défausse et fuir*/
+  SDL_RenderCopy(rendu, defausse_texture, NULL, &defausse_rect);
+  SDL_RenderCopy(rendu, fuir_texture, NULL, &fuir_rect);
+
+  /*Mise en place du personnage*/
+  int w=pers->sprites[idle_droite].rectangle.w;
+  int h=pers->sprites[idle_droite].rectangle.h;
+  int x=pers->sprites[idle_droite].rectangle.x;
+  int y=pers->sprites[idle_droite].rectangle.y;
+  pers->sprites[idle_droite].rectangle.w *=3;
+  pers->sprites[idle_droite].rectangle.h *=3;
+  pers->sprites[idle_droite].rectangle.x = 150;
+  pers->sprites[idle_droite].rectangle.y = 250;
+  pers->sprites[courant] = pers->sprites[idle_droite];
+  SDL_RenderCopy(rendu, pers->sprites[courant].img, NULL, &pers->sprites[0].rectangle);
+
+  /*Afficher l'ennemi*/
+  int xe=ennemi->sprites[0].rectangle.x;
+  int ye=ennemi->sprites[0].rectangle.y;
+  int he=ennemi->sprites[0].rectangle.h;
+  int we=ennemi->sprites[0].rectangle.w;
+  ennemi->sprites[0].rectangle.x = 750;
+  ennemi->sprites[0].rectangle.y = 240;
+  ennemi->sprites[0].rectangle.w *= 3;
+  ennemi->sprites[0].rectangle.h *= 3;
+  ennemi->sprites[0] = ennemi->sprites[0];
+  SDL_RenderCopy(rendu, ennemi->sprites[0].img, NULL, &ennemi->sprites[0].rectangle);
+
+  SDL_RenderPresent(rendu);
+  pers->sprites[idle_droite].rectangle.w = w;
+  pers->sprites[idle_droite].rectangle.h = h;
+  pers->sprites[idle_droite].rectangle.x = x+10;
+  pers->sprites[idle_droite].rectangle.y = y+10;
+  ennemi->sprites[0].rectangle.w = we;
+  ennemi->sprites[0].rectangle.h = he;
+  ennemi->sprites[0].rectangle.x = xe-50;
+  ennemi->sprites[0].rectangle.y = ye+50;
+}
 
 
 
@@ -205,7 +200,7 @@ int combat_t_p_t(perso_t * perso, ennemi_t * ennemi,SDL_Renderer *rendu)
   rectangle_selection->x = (def.rectangle).x - RECT_SELECT_X_DIFF;
 	rectangle_selection->y = (def.rectangle).y - RECT_SELECT_Y_DIFF;
 	rectangle_selection->w = (def.rectangle).w +100;
-	rectangle_selection->h = (def.rectangle).h +50;
+	rectangle_selection->h = (def.rectangle).h +30;
 
   init_liste();
   ajout_droit(creer_carte("soin", DEFENSE, 5, 0));
@@ -215,7 +210,7 @@ int combat_t_p_t(perso_t * perso, ennemi_t * ennemi,SDL_Renderer *rendu)
   int choix=0, i, vitesse,fuire=1;
   vitesse = perso->vitesse;
   while((ennemi->pv > 0 && perso->pv > 0) && fuire==1 ){
-    affichage_combat_personnage(rendu,perso,ennemi,def.img,fui.img,def.rectangle,fui.rectangle);
+    affichage_combat_personnage(rendu,perso,ennemi,def.img,fui.img,def.rectangle,fui.rectangle,rectangle_selection);
     choix=deplacement_rectangle_selection_combat(def.rectangle,fui.rectangle,&rectangle_selection);
     printf("Vous avez %d pv et le %s a %d pv\n",perso->pv, ennemi->nom, ennemi->pv);
     printf("Vous avez %d de vitesse et le %s a %d de vitesse\n",perso->vitesse,ennemi->nom, ennemi->vitesse);
